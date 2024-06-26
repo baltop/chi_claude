54bfec8d1d29df8b9421e8f69a5812e848093396
baltop <baltop@gmail.com>
2024-06-26 15:19:39 +0900

    라우터를 설정값으로 생성하는 부분 완료

--- main.go
+++ main.go
@@ -55,126 +55,6 @@ var apiKeys = []string{
 	"ed0caae6-0b4b-408f-b300-1545e05db00a",
 }
 
-func main() {
-
-	plugins := map[string]func(http.Handler) http.Handler{
-		"apiKeyMiddleware":      apiKeyMiddleware,
-		"stripPrefixMiddleware": stripPrefixMiddleware,
-	}
-
-	routerDefinitions := []*RouterDefinition{
-		NewRouterDefinition("/api/service-a", "https://httpbin.org", []string{"apiKeyMiddleware"}, nil),
-		NewRouterDefinition("/api/service-b", "https://httpbin.org", []string{"stripPrefixMiddleware"}, nil),
-		NewRouterDefinition("/api/service-c", "https://httpbin.org", nil, nil), // no middleware
-	}
-
-	for _, r := range routerDefinitions {
-		for _, mwname := range r.mwname {
-			if mw, ok := plugins[mwname]; ok {
-				r.addMiddleware(mw)
-			}
-		}
-	}
-
-	r := chi.NewRouter()
-
-	// Middleware
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(metricsMiddleware)
-
-	// Routes
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to the API Gateway"))
-	})
-
-	r.Route("/api", func(r chi.Router) {
-
-		for _, def := range routerDefinitions {
-			url, _ := url.Parse(def.Upstream)
-			// method가 * 이면?   r.With(def.middleware...).HandleFunc(def.ListenPath, proxyHandlerB(url))
-			r.With(def.middleware...).Method(def.Method, def.ListenPath, proxyHandlerB(url))
-		}
-
-		// routerDefinitions를 이용한 위 코드면 완성.  아래는 그냥 참고용 샘플 코드.
-		r.With(stripPrefixMiddleware).Get("/service-b/*", proxyHandlerB(serviceB))
-
-		r.With(apiKeyMiddleware).Get("/left/*", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("left road"))
-		})
-
-		// url이 긴거 부터 먼저 매칭됨
-		r.With(apiKeyMiddleware).Get("/left/east/*", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("left east road"))
-		})
-
-		// mount 방식은 새로운 라우터를 만드는데, 모든 메소드를 컨트롤
-		r.Mount("/service-a", apiKeyMiddleware(http.StripPrefix("/api/service-a", proxyHandlerA(serviceA))))
-		r.With(apiKeyMiddleware).Mount("/service-c", http.StripPrefix("/api/service-c", proxyHandlerA(serviceA)))
-	})
-
-	r.Handle("/metrics", promhttp.Handler())
-
-	// Start server
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	fmt.Printf("Server is running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
-}
-
-func proxyHandlerA(target *url.URL) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		proxy := httputil.NewSingleHostReverseProxy(target)
-		proxy.ServeHTTP(w, r)
-	})
-}
-
-func proxyHandlerB(target *url.URL) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		proxy := httputil.NewSingleHostReverseProxy(target)
-		proxy.ServeHTTP(w, r)
-	})
-}
-
-func metricsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		next.ServeHTTP(w, r)
-
-		duration := time.Since(start).Seconds()
-		httpRequestsTotal.WithLabelValues(r.Method, r.URL.Path).Inc()
-		httpRequestDuration.WithLabelValues(r.Method, r.URL.Path).Observe(duration)
-	})
-}
-
-func apiKeyMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		apiKey := r.Header.Get("X-API-Key")
-		for _, key := range apiKeys {
-			if apiKey == key {
-				next.ServeHTTP(w, r)
-				return
-			}
-		}
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-	})
-}
-
-func stripPrefixMiddleware(next http.Handler) http.Handler {
-	prefix := "/api/service-b"
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if p := strings.TrimPrefix(r.URL.Path, prefix); len(p) < len(r.URL.Path) {
-			r.URL.Path = p
-			next.ServeHTTP(w, r)
-		} else {
-			http.NotFound(w, r)
-		}
-	})
-}
-
 type RouterDefinition struct {
 	ListenPath string
 	Method     string
@@ -194,4 +74,144 @@ func NewRouterDefinition(listenPath, method, upstream string, mwname []string, m
 
 func (r *RouterDefinition) addMiddleware(middleware func(http.Handler) http.Handler) {
 	r.middleware = append(r.middleware, middleware)
+}
+
+// main 함수
+func main() {
+
+	// 미들웨어 플러그인의 설정값을 읽어서 map에 저장
+	plugins := map[string]func(http.Handler) http.Handler{
+		"apiKeyMiddleware":      apiKeyMiddleware,
+		"stripPrefixMiddleware": stripPrefixMiddleware,
+	}
+
+	// reouterDefinition을 설정파일이나 DB에서 읽어와서 저장
+	routerDefinitions := []*RouterDefinition{
+		NewRouterDefinition("/api/service-a", "GET", "https://httpbin.org", []string{"apiKeyMiddleware"}, nil),
+		NewRouterDefinition("/api/service-b", "POST", "https://httpbin.org", []string{"stripPrefixMiddleware"}, nil),
+		NewRouterDefinition("/api/service-c", "*", "https://httpbin.org", nil, nil), // no middleware
+	}
+
+	// 스트링으로 된 설정값의 미들웨어 명을 실제 미들웨어 함수의 슬라이스로 변환하여 routerDefinition에 저장
+	for _, r := range routerDefinitions {
+		for _, mwname := range r.mwname {
+			if mw, ok := plugins[mwname]; ok {
+				r.addMiddleware(mw)
+			}
+		}
+	}
+
+	// default root router
+	r := chi.NewRouter()
+
+	// 전역으로 사용될 Middleware. api 라우터 별로 설정되는 미들웨어는 플러그인이라고 부르고 routerDefinition에 따라 개별 저장
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(metricsMiddleware)
+
+	// 디폴트 테스트 Routes
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the API Gateway"))
+	})
+
+	// /api의 서브 라우터 설정
+	r.Route("/api", func(r chi.Router) {
+
+		for _, def := range routerDefinitions {
+			url, _ := url.Parse(def.Upstream)
+			// method가 * 이면?   r.With(def.middleware...).HandleFunc(def.ListenPath, proxyHandlerB(url))
+			if def.Method == "*" {
+				r.With(def.middleware...).HandleFunc(def.ListenPath, proxyHandlerB(url))
+				continue
+			}
+			r.With(def.middleware...).Method(def.Method, def.ListenPath, proxyHandlerB(url))
+		}
+
+		//
+		// routerDefinitions를 이용한 위 코드면 완성.  아래는 그냥 참고용 샘플 코드.
+		//
+		r.With(stripPrefixMiddleware).Get("/service-b/*", proxyHandlerB(serviceB))
+
+		// 아래와 비교
+		r.With(apiKeyMiddleware).Get("/left/*", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("left road"))
+		})
+
+		// url이 긴거 부터 먼저 매칭됨
+		r.With(apiKeyMiddleware).Get("/left/east/*", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("left east road"))
+		})
+
+		// mount 방식은 새로운 라우터를 만드는데, 메소드를 붙일 수 없이 전부 다 적용되고 listenPath에 * 없는데 주의할 것.
+		r.Mount("/service-a", apiKeyMiddleware(http.StripPrefix("/api/service-a", proxyHandlerA(serviceA))))
+		r.With(apiKeyMiddleware).Mount("/service-c", http.StripPrefix("/api/service-c", proxyHandlerA(serviceA)))
+	})
+
+	// Prometheus metrics handler 프로메테우스 서버에서 데이터 수집할때 접근하는 url
+	r.Handle("/metrics", promhttp.Handler())
+
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("Server is running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+
+}
+
+// retrun http.Handler  interface
+func proxyHandlerA(target *url.URL) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy := httputil.NewSingleHostReverseProxy(target)
+		proxy.ServeHTTP(w, r)
+	})
+}
+
+// return http.HandlerFunc  type
+func proxyHandlerB(target *url.URL) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy := httputil.NewSingleHostReverseProxy(target)
+		proxy.ServeHTTP(w, r)
+	})
+}
+
+// prometheus 메트릭스를 수집하기 위한 middleware
+func metricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(start).Seconds()
+		httpRequestsTotal.WithLabelValues(r.Method, r.URL.Path).Inc()
+		httpRequestDuration.WithLabelValues(r.Method, r.URL.Path).Observe(duration)
+	})
+}
+
+// apikey를 체크하기 위한 미들웨어
+func apiKeyMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-Key")
+		for _, key := range apiKeys {
+			if apiKey == key {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	})
+}
+
+// upstream으로 보내기 전에 url에서 prefix를 제거하기 위한 미들웨어
+func stripPrefixMiddleware(next http.Handler) http.Handler {
+	prefix := "/api/service-b"
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if p := strings.TrimPrefix(r.URL.Path, prefix); len(p) < len(r.URL.Path) {
+			r.URL.Path = p
+			next.ServeHTTP(w, r)
+		} else {
+			http.NotFound(w, r)
+		}
+	})
 }
